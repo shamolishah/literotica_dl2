e7688921e108b2285be6227a218ce2c1a2d8dad2
Shamoli Shah <shamolishah85@gmail.com>
2024-09-04 20:29:43 -0500

    feat: :sparkles:
    Add better cli with typer

---
+++ src/literotica_dl2/literotica_dl2.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+
+import typer
+
+from literotica_dl2.__about__ import __version__
+from literotica_dl2.author import Author
+from literotica_dl2.story import Story
+from literotica_dl2.story_series import StorySeries
+from literotica_dl2.utils import OutputFormat, save_story
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(style="{", fmt="[{name}:{filename}] {levelname} - {message}"))
+
+log = logging.getLogger("literotica_dl")
+log.setLevel(logging.INFO)
+log.addHandler(handler)
+
+app = typer.Typer()
+
+
+@app.command()
+def version() -> None:
+    log.info("The app Version is %s", __version__)
+
+
+@app.command()
+def story(story_slug: str, output: str = "output", output_format: OutputFormat = "txt") -> None:
+    log.info("Trying to get %s", story_slug)
+    Path(output).mkdir(exist_ok=True, parents=False)
+    story = Story(story_slug)
+    save_story(output, story, output_format=output_format)
+
+
+@app.command()
+def author(author_slug: str, output: str = "output") -> None:
+    log.info("Trying to get the author: %s", author_slug)
+    Path(output).mkdir(exist_ok=True, parents=False)
+    author = Author(author_slug)
+    for story_stub in author.individual_stories:
+        story = Story(story_stub)
+        save_story(output, story, extra_folder="Individual Stories")
+    for series_stub in author.series:
+        series = StorySeries(series_stub)
+        for chapter_stub in series.stories:
+            chapter = Story(chapter_stub)
+            save_story(output, chapter, extra_folder=series.title)
+
+
+if __name__ == "__main__":
+    app()
--- src/literotica_dl2/utils.py
+++ src/literotica_dl2/utils.py
@@ -1,6 +1,10 @@
+from __future__ import annotations
+
 import logging
 import re
 from datetime import timedelta
+from enum import Enum
+from pathlib import Path
 
 import requests_cache
 from fake_useragent import UserAgent
@@ -31,3 +35,25 @@ def parse_story_series(url: str) -> str:
 def parse_story_url(url: str) -> str:
     series_url_matcher = re.compile(r".+\/s\/(.+)")
     return series_url_matcher.match(url).groups()[0]
+
+
+def save_story_pre(base_folder: str, story, extra_folder: str | None) -> Path:
+    if extra_folder is not None:
+        dst_path = Path(base_folder) / get_sane_filename(story.author) / get_sane_filename(extra_folder)
+    else:
+        dst_path = Path(base_folder) / get_sane_filename(story.author)
+    dst_path.mkdir(exist_ok=True, parents=True)
+    return dst_path
+
+
+def save_story(base_folder: str, story, extra_folder: str | None = None, output_format: OutputFormat = "txt") -> None:
+    dst_path = save_story_pre(base_folder=base_folder, story=story, extra_folder=extra_folder)
+    if output_format == OutputFormat.txt:
+        story_path = (dst_path / get_sane_filename(story.title)).with_suffix(".txt")
+        story_path.write_text(story.text)
+
+
+class OutputFormat(Enum):
+    txt = "txt"
+    Markdown = "md"
+    HTML = "html"
